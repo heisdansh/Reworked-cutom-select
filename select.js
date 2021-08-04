84b71adc3215b006472ae8072aa56b320798fffd
heisdansh <63120880+heisdansh@users.noreply.github.com>
2021-08-04 16:15:41 +0300

    Final?

--- select.js
+++ select.js
@@ -10,11 +10,16 @@ export default class Select {
         element.after(this.customElement)
     }
 
+    get selectedOptionIndex() {
+        return this.options.indexOf(this.selectedOption)
+    }
+
     get selectedOption() {
         return this.options.find(option => option.selected)
     }
 
     selectValue(value) {
+        const selected = ['bg-blue-600']
         const newSelectedOption = this.options.find(option =>{
             return option.value === value
         })
@@ -26,12 +31,20 @@ export default class Select {
         newSelectedOption.element.selected = true
 
         this.labelElement.innerText = newSelectedOption.label
+
+        this.optionsCustomElement.querySelector(
+            `[data-value="${prevSelectedOption.value}"]`
+                ).classList.remove(...selected)
+        const newCustomElement = this.optionsCustomElement.querySelector(`[data-value="${newSelectedOption.value}"]`)
+        newCustomElement.classList.add(...selected)
+        newCustomElement.scrollIntoView({block: 'nearest'})
+
     }
 }
 function setupCustomElement(select) {
     const customSelectContainer = ['cutom-select-container','box-border','flex','flex-col']
     const customSelectValue = ['cutom-select-value','box-border','bg-black','text-white','text-center','text-lg','font-bold','px-6','py-1','rounded-md','cursor-pointer','select-none','focus:border-4','focus:outline-none','border-2','border-white']
-    const customSelectOptions = ['cutom-select-options','box-border','flex','flex-col','align-center','width-full','border-2','border-black','bg-red-400','rounded-b-md','max-h-24','overflow-auto']
+    const customSelectOptions = ['cutom-select-options','box-border','flex','flex-col','align-center','width-full','border-2','border-black','bg-red-400','rounded-b-md','max-h-24','overflow-auto','absolute/neveikia']
     const customSelectOption = ['custom-select-option','box-border','py-1','hover:bg-blue-300','cursor-pointer']
     const selected = ['bg-blue-600']
     select.customElement.classList.add(...customSelectContainer)
@@ -49,16 +62,12 @@ function setupCustomElement(select) {
         optionElement.innerText = option.label
         optionElement.dataset.value = option.value
         optionElement.addEventListener('click',()=>{
-            select.optionsCustomElement.querySelector(
-            `[data-value="${select.selectedOption.value}"]`
-                ).classList.remove(...selected)
-            //select.selectedOption.element.classList.remove(...selected)
             select.selectValue(option.value)
-            optionElement.classList.add(...selected)
             select.optionsCustomElement.classList.add('hidden')
         })
         select.optionsCustomElement.append(optionElement)
     })
+    select.optionsCustomElement.classList.add('hidden')
     select.customElement.append(select.optionsCustomElement)
 
     select.labelElement.addEventListener('click',()=>{
@@ -66,7 +75,44 @@ function setupCustomElement(select) {
     })
 
     select.customElement.addEventListener("blur",() => {
-        select.optionsCustomElement.classList.remove('hidden')
+        select.optionsCustomElement.classList.add('hidden')
+    })
+    let debounceTimeout
+    let searchTerm = ""
+    select.customElement.addEventListener("keydown", e => {
+        switch (e.code){
+            case "Space":
+                select.optionsCustomElement.classList.toggle('hidden')
+                break
+            case "ArrowUp":
+                const prevOption = select.options[select.selectedOptionIndex - 1]
+                if(prevOption){
+                    select.selectValue(prevOption.value)
+                }
+                break
+            case "ArrowDown":
+                const nextOption = select.options[select.selectedOptionIndex + 1]
+                if(nextOption){
+                    select.selectValue(nextOption.value)
+                }
+                break
+            case "Enter":
+            case "Escape":
+                select.optionsCustomElement.classList.add('hidden')
+                break
+            default:
+                clearTimeout(debounceTimeout)
+                searchTerm += e.key
+                debounceTimeout = setTimeout(() => {
+                    searchTerm = ""
+                }, 500);
+
+                const searchedOption = select.options.find(option =>{
+                    return option.label.toLowerCase().startsWith(searchTerm)
+                })
+
+                if (searchedOption) select.selectValue(searchedOption.value)
+        }
     })
 }
 
